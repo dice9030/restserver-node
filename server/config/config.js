051d519b5326f7e45d7be5cd35b2b52b4af7ce74
dcelis <dcelis@sofdynamic.org>
2019-07-23 15:54:55 -0500

    archivos variables de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://dice9030:JqG3973vA7hH58C@cluster0-6p8qf.gcp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
